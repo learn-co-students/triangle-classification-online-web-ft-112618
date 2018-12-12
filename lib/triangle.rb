dfb11c627559624f371bda7bbd42972ad8165b4f
Henry <hanke.liu@gmail.com>
2018-12-11 23:28:04 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c #:equilateral, :isosceles, :scalene
+
+  def initialize(a,b,c)
+    # hash.each do |method, value|
+    #   self.send("#{method}=", value)
+    # end
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+  def kind
+    self.validate
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  def validate
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a + b <= c || a + c <= b || b + c <= a
+      raise TriangleError
+    else
+      true
+    end 
+  end
+
+
+  class TriangleError < StandardError
+
+  end
+
 end
