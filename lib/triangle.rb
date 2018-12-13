8fe86adaa76e7218bfab76af439b271b6ef2380f
Nick Inman <nick@theoatmeal.com>
2018-12-13 19:06:59 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a 
+    @side_b = side_b
+    @side_c = side_c
+  end 
+  
+  def kind 
+    if side_a + side_b <= side_c || side_c + side_a <= side_b ||  side_c + side_b <= side_a || side_a <= 0 || side_b <= 0 || side_c <= 0
+      begin 
+        raise TriangleError
+      rescue TriangleError => error 
+        puts error.message 
+        raise
+      end 
+      elsif side_a == side_b && side_a == side_c
+        :equilateral
+      elsif side_a == side_b || side_a == side_c || side_b == side_c
+        :isosceles
+      elsif side_a != side_b && side_a != side_c
+        :scalene
+    end 
+  end 
+  
+  
+  
+  class TriangleError < StandardError
+    def message
+      "There is an issue with this triangle.."
+    end 
+  end
 end
